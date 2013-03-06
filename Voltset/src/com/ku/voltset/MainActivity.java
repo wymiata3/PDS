1bededa0871bbc04db003ee52cc2e93a526a385c
chmod <chmod@localhost.localdomain>
2013-03-06 13:58:43 +0100

    Added second activity (Settings) with Seekbar to chage duration Button "settings" in main activity Changed theme to holo Visual enhancements on changing activity

--- Voltset/src/com/ku/voltset/MainActivity.java
+++ Voltset/src/com/ku/voltset/MainActivity.java
@@ -1,113 +1,121 @@
 package com.ku.voltset;
 
+
 import com.example.helloworld.R;
 
 import android.os.Bundle;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
-import android.annotation.SuppressLint;
+import android.os.Handler;
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MotionEvent;
+import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnTouchListener;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
-import android.view.animation.DecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener,AnimationListener {
-	private double lastMeasurement = 0.000;
+public class MainActivity extends Activity implements OnClickListener,
+		AnimationListener {
+	private double lastMeasurement = 0.00;
 	private ImageView image;
-	
+	float currentValue = 0.00f;
+	final Handler mHandler = new Handler();
+	private int duration=-1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		image = (ImageView) findViewById(R.id.imageArrow);
 		Button simButton = (Button) findViewById(R.id.simButton);
+		Button settingsButton = (Button) findViewById(R.id.buttonSettings);
+		image = (ImageView) findViewById(R.id.imageArrow);
+		duration= this.getIntent().getIntExtra("duration",-1);
 		simButton.setOnClickListener(this);
+		settingsButton.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View view) {
 
+		if (view.getId() == R.id.buttonSettings) {
+			Intent settingsActivity = new Intent(this, SettingsActivity.class);
+			settingsActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			if(duration==-1)
+				duration=2500;
+			settingsActivity.putExtra("duration",duration);
+			startActivity(settingsActivity);
+			overridePendingTransition(R.anim.left_to_right,
+					R.anim.right_to_left);
+		}
 		if (view.getId() == R.id.simButton) {
-			
-			EditText editText = (EditText) findViewById(R.id.simText);
+			final EditText editText = (EditText) findViewById(R.id.simText);
 			TextView prevText = (TextView) findViewById(R.id.prevText);
-			TextView mVoltsText = (TextView) findViewById(R.id.mVoltsText);
-			if(editText.getText().length()==0){
-				Toast.makeText(getApplicationContext(), "Input can't be empty", Toast.LENGTH_SHORT).show();
+			final TextView mVoltsText = (TextView) findViewById(R.id.mVoltsText);
+			
+			if (editText.getText().length() == 0) {
+				Toast.makeText(getApplicationContext(), "Input can't be empty",
+						Toast.LENGTH_SHORT).show();
 				return;
 			}
-			if((Float.valueOf(editText.getText().toString())<0) || (Float.valueOf(editText.getText().toString())>180))
-			{
-				Toast.makeText(getApplicationContext(), "Unaccepted value (0-180)", Toast.LENGTH_SHORT).show();
+			if ((Float.valueOf(editText.getText().toString()) < 0)
+					|| (Float.valueOf(editText.getText().toString()) > 180)) {
+				Toast.makeText(getApplicationContext(),
+						"Unaccepted value (0-180)", Toast.LENGTH_SHORT).show();
 				return;
 			}
-			if (lastMeasurement == 0.000) {
-				lastMeasurement = Double.valueOf(editText.getText().toString());
+			
+			
+			if (lastMeasurement == 0.00) {
+				prevText.setText("-");
+				
+			} else {
+				lastMeasurement=Float.valueOf(mVoltsText.getText()
+						.toString().substring(0,mVoltsText.getText()
+						.toString().indexOf("Volts")));
+				String result = String.format("%.2f",lastMeasurement);
+				prevText.setText(result);
 			}
-			else
-				lastMeasurement = Double.valueOf(mVoltsText.getText().toString());
-			prevText.setText(String.valueOf(lastMeasurement));
-			mVoltsText.setText(editText.getText().toString());
-			//Animation start
-			float Xaxis=0.835366f;
-			float Yaxis=0.676692f;
-			RotateAnimation rotateAnimation1 = new RotateAnimation(0, Float.valueOf(editText.getText().toString()),
+			
+			if(duration==-1)
+				duration=2500;
+			// Animation start
+			float Xaxis = 0.835366f;
+			float Yaxis = 0.676692f;
+			RotateAnimation rotateAnimation1 = new RotateAnimation(0,
+					Float.valueOf(editText.getText().toString()),
 					Animation.RELATIVE_TO_SELF, Xaxis,
 					Animation.RELATIVE_TO_SELF, Yaxis);
 			rotateAnimation1.setInterpolator(new LinearInterpolator());
-			rotateAnimation1.setDuration(2500);
+			rotateAnimation1.setDuration(duration);
 			rotateAnimation1.setRepeatCount(0);
-			
 			rotateAnimation1.setAnimationListener(this);
-			image.clearAnimation();
-			image.invalidate();
-			rotateAnimation1.cancel();
-			rotateAnimation1.reset();
 			image.startAnimation(rotateAnimation1);
-			    
-			
+			String result = String.format("%.2f",Float.parseFloat(editText.getText().toString()));
+			mVoltsText.setText(result+"Volts");
+			lastMeasurement=Float.valueOf(mVoltsText.getText()
+					.toString().substring(0,mVoltsText.getText()
+					.toString().indexOf("Volts")));
+
 		}
 	}
 
 	@Override
 	public void onAnimationEnd(Animation animation) {
 		animation.setFillAfter(true);
-		
-		
-		
 	}
 
 	@Override
 	public void onAnimationRepeat(Animation animation) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void onAnimationStart(Animation animation) {
-		
+
 	}
 }
---
+++ Voltset/src/com/ku/voltset/SettingsActivity.java
@@ -0,0 +1,66 @@
+package com.ku.voltset;
+
+import com.example.helloworld.R;
+import com.example.helloworld.R.layout;
+import com.example.helloworld.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Toast;
+
+public class SettingsActivity extends Activity implements OnSeekBarChangeListener {
+	private int duration;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+		int duration= this.getIntent().getIntExtra("duration",0);
+		this.duration=duration;
+		SeekBar durationLength=(SeekBar)findViewById(R.id.seekBar);
+		durationLength.setProgress(duration);
+		durationLength.setOnSeekBarChangeListener(this);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.settings, menu);
+		return true;
+	}
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)  {
+	    if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+	        Intent mainActivity = new Intent(SettingsActivity.this,MainActivity.class);    
+	        mainActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	        mainActivity.putExtra("duration",duration);
+	        startActivity(mainActivity);
+	        return true;
+	    }
+
+	    return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+		duration=progress;
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
