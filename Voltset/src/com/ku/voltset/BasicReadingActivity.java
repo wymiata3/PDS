ec8f73356f110ea955c8ab2d2b32232527378e3d
chmod <chmod@localhost.localdomain>
2013-04-04 17:38:04 +0200

    Create class for logging. Added test button on BasicActivity just for showing log file

--- Voltset/src/com/ku/voltset/BasicReadingActivity.java
+++ Voltset/src/com/ku/voltset/BasicReadingActivity.java
@@ -1,5 +1,15 @@
 package com.ku.voltset;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import com.yoctopuce.YoctoAPI.*;
 
 import com.ku.voltset.R;
@@ -7,6 +17,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.Animation;
@@ -34,6 +45,7 @@ public class BasicReadingActivity extends Activity implements OnClickListener {
 	RotateAnimation rotate = null; // Rotate animation
 	TextView mVoltsText; // Current measurement
 	TextView prevText;// Previous measurement
+	Logger log;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +59,14 @@ public class BasicReadingActivity extends Activity implements OnClickListener {
 		settingsButton.setOnClickListener(this);
 		mVoltsText = (TextView) findViewById(R.id.mVoltsText);
 		prevText = (TextView) findViewById(R.id.prevText);
+		log = new Logger(this);
+		log.setFile("VoltSet.csv");
 		handler = new Handler();
 		handler.postDelayed(r, 500); // Start measuring at start of activity
-
+		
+		//Testing purposes button only.
+		Button testButton = (Button) findViewById(R.id.button1);
+		testButton.setOnClickListener(this);
 	}
 
 	@Override
@@ -63,6 +80,25 @@ public class BasicReadingActivity extends Activity implements OnClickListener {
 			startActivity(settingsActivity);
 			overridePendingTransition(R.anim.left_to_right,
 					R.anim.right_to_left);
+		}
+		if (view.getId() == R.id.button1) {
+			try {
+				FileInputStream fstream = new FileInputStream(log.getFile());
+				DataInputStream in = new DataInputStream(fstream);
+				BufferedReader br = new BufferedReader(
+						new InputStreamReader(in));
+				String strLine;
+				// Read File Line By Line
+				while ((strLine = br.readLine()) != null) {
+					// Print the content on the console
+					Toast.makeText(getApplicationContext(), strLine, 1000).show();
+				}
+				
+				// Close the input stream
+				in.close();
+			} catch (IOException io) {
+				io.printStackTrace();
+			}
 		}
 	}
 
@@ -89,7 +125,9 @@ public class BasicReadingActivity extends Activity implements OnClickListener {
 
 		}
 	};
-
+	protected static String getTimeStamp(){
+		return java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+	}
 	/**
 	 * Runnable binded to handler
 	 * 
@@ -108,22 +146,29 @@ public class BasicReadingActivity extends Activity implements OnClickListener {
 
 				try {
 					currentMeasurement = Float.parseFloat((String.format(
-							"%.1f", dc_sensor.getCurrentValue())));//We want decimal values
-					if (currentMeasurement < 0) { //alert to reverse the polarity
+							"%.1f", dc_sensor.getCurrentValue())));// We want
+																	// decimal
+																	// values
+					if (currentMeasurement < 0) { // alert to reverse the
+													// polarity
 						Toast.makeText(getApplicationContext(),
 								"Reverse your cables!", Toast.LENGTH_SHORT)
 								.show();
 						handler.postDelayed(this, 2000);
 						return;
-					} else if (currentMeasurement == 0) {//No more measuring
+					} else if (currentMeasurement == 0) {// No more measuring
 						if (rotate != null) {
 							rotate.cancel();
 							lastMeasurement = 0.0f;
 							mVoltsText.setText("0.0V");
 						}
 					} else if (currentMeasurement == lastMeasurement) {
-						// Same measurement, do nothing
-					} else if (currentMeasurement > lastMeasurement) { //Probably new measurement, catch it
+						log.write("EVENT:"+getTimeStamp()+" Measurement:" + currentMeasurement + "DC");
+					} else if (currentMeasurement > lastMeasurement) { // Probably
+																		// new
+																		// measurement,
+																		// catch
+																		// it
 						rotate = new RotateAnimation(0,
 								Float.valueOf(currentMeasurement * 36),
 								Animation.RELATIVE_TO_SELF, Xaxis,
@@ -140,9 +185,11 @@ public class BasicReadingActivity extends Activity implements OnClickListener {
 								dc_sensor.getCurrentValue(),
 								dc_sensor.getUnit())
 								+ "V");
-					} else {
+
+					} else {						
+						
 						rotate = new RotateAnimation(
-								Float.valueOf(currentMeasurement * 36),0,
+								Float.valueOf(currentMeasurement * 36), 0,
 								Animation.RELATIVE_TO_SELF, Xaxis,
 								Animation.RELATIVE_TO_SELF, Yaxis);
 						rotate.setInterpolator(new LinearInterpolator());
@@ -155,16 +202,17 @@ public class BasicReadingActivity extends Activity implements OnClickListener {
 				} catch (YAPI_Exception e) {
 					e.printStackTrace();
 				}
-//				YVoltage ac_sensor = YVoltage.FindVoltage(serial + ".voltage2");//AC sensor, to be taken by HardwareController
-//				try {
-//					TextView view = (TextView) findViewById(R.id.acVolt);
-//					view.setText(String.format("%.1f %s",
-//							ac_sensor.getCurrentValue(), ac_sensor.getUnit()));
-//				} catch (YAPI_Exception e) {
-//					e.printStackTrace();
-//				}
+				// YVoltage ac_sensor = YVoltage.FindVoltage(serial +
+				// ".voltage2");//AC sensor, to be taken by HardwareController
+				// try {
+				// TextView view = (TextView) findViewById(R.id.acVolt);
+				// view.setText(String.format("%.1f %s",
+				// ac_sensor.getCurrentValue(), ac_sensor.getUnit()));
+				// } catch (YAPI_Exception e) {
+				// e.printStackTrace();
+				// }
 			}
-			handler.postDelayed(this, 2000);//Run again in 2sec
+			handler.postDelayed(this, 2000);// Run again in 2sec
 		}
 	};
 }
--- Voltset/src/com/ku/voltset/VoltSet.java
+++ Voltset/src/com/ku/voltset/VoltSet.java
@@ -1,11 +1,33 @@
 package com.ku.voltset;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import com.ku.voltset.services.HardwareController_service;
 import com.yoctopuce.YoctoAPI.YAPI;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,10 +41,32 @@ public class VoltSet extends Activity implements OnClickListener {
 	final Context context = this;
 	AlphaAnimation aa;
 	public ImageView infoIcon;
+	Messenger mService = null;
+	boolean mIsBound;
+	final Messenger mMessenger = new Messenger(new IncomingHandler());
+
 	@Override
 	protected void onStart() {
 		super.onStart();
-		hc = new HardwareController(context,this);
+		// doBindService();
+		Log.d("YOCTOPUS_SERVICE", "startt");
+		if (hc == null) {
+			hc = new HardwareController(context, this);
+		}
+	}
+
+	static class IncomingHandler extends Handler {
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case HardwareController_service.MSG_SET_VALUE:
+				Log.d("YOCTOPUS_SERVICE",
+						String.valueOf("Message arrived:" + msg.arg1));
+				break;
+			default:
+				super.handleMessage(msg);
+			}
+		}
 	}
 
 	@Override
@@ -34,25 +78,27 @@ public class VoltSet extends Activity implements OnClickListener {
 	protected void onDestroy() {
 		super.onDestroy();
 		try {
+			hc.terminate();
 			YAPI.FreeAPI();
 		} catch (Exception e) {
 			// Free no matter what
 		}
 	}
 
-	//Unused animations, future maybe?
-//	public static Animation runFadeOutAnimationOn(Activity ctx, View target) {
-//		Animation animation = AnimationUtils.loadAnimation(ctx,
-//				R.anim.fadeout);
-//		target.startAnimation(animation);
-//		return animation;
-//	}
-//	public static Animation runFadeInAnimationOn(Activity ctx, View target) {
-//		Animation animation = AnimationUtils.loadAnimation(ctx,
-//				R.anim.fedein);
-//		target.startAnimation(animation);
-//		return animation;
-//	}
+	// Unused animations, future maybe?
+	// public static Animation runFadeOutAnimationOn(Activity ctx, View target)
+	// {
+	// Animation animation = AnimationUtils.loadAnimation(ctx,
+	// R.anim.fadeout);
+	// target.startAnimation(animation);
+	// return animation;
+	// }
+	// public static Animation runFadeInAnimationOn(Activity ctx, View target) {
+	// Animation animation = AnimationUtils.loadAnimation(ctx,
+	// R.anim.fedein);
+	// target.startAnimation(animation);
+	// return animation;
+	// }
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -64,8 +110,12 @@ public class VoltSet extends Activity implements OnClickListener {
 		conf.setOnClickListener(this);
 		Button quit = (Button) findViewById(R.id.quit);
 		quit.setOnClickListener(this);
-		infoIcon=(ImageView)findViewById(R.id.infoIcon);
+		infoIcon = (ImageView) findViewById(R.id.infoIcon);
 		infoIcon.setEnabled(false);
+		//try to rotate log if too big or too old
+		Logger log=new Logger(this);
+		log.setFile("VoltSet.csv");
+		log.logRotate(25L,100);
 	}
 
 	@Override
@@ -112,4 +162,79 @@ public class VoltSet extends Activity implements OnClickListener {
 
 	}
 
+	private ServiceConnection mConnection = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			// This is called when the connection with the service has been
+			// established, giving us the service object we can use to
+			// interact with the service. We are communicating with our
+			// service through an IDL interface, so get a client-side
+			// representation of that from the raw service object.
+			mService = new Messenger(service);
+			Log.d("YOCTOPUS_SERVICE", "Attached.");
+
+			// We want to monitor the service for as long as we are
+			// connected to it.
+			try {
+				Message msg = Message.obtain(null,
+						HardwareController_service.MSG_REGISTER_CLIENT);
+				msg.replyTo = mMessenger;
+				mService.send(msg);
+
+				// Give it some value as an example.
+				msg = Message.obtain(null,
+						HardwareController_service.MSG_SET_VALUE,
+						this.hashCode(), 0);
+				mService.send(msg);
+			} catch (RemoteException e) {
+				// In this case the service has crashed before we could even
+				// do anything with it; we can count on soon being
+				// disconnected (and then reconnected if it can be restarted)
+				// so there is no need to do anything here.
+			}
+
+			// As part of the sample, tell the user what happened.
+			Log.d("YOCTOPUS_SERVICE", "Connected.");
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			// This is called when the connection with the service has been
+			// unexpectedly disconnected -- that is, its process crashed.
+			mService = null;
+			Log.d("YOCTOPUS_SERVICE", "Disconnected.");
+
+		}
+	};
+
+	void doBindService() {
+		// Establish a connection with the service. We use an explicit
+		// class name because there is no reason to be able to let other
+		// applications replace our component.
+		bindService(new Intent(VoltSet.this, HardwareController_service.class),
+				mConnection, Context.BIND_AUTO_CREATE);
+		mIsBound = true;
+		Log.d("YOCTOPUS_SERVICE", "Binding.");
+	}
+
+	void doUnbindService() {
+		if (mIsBound) {
+			// If we have received the service, and hence registered with
+			// it, then now is the time to unregister.
+			if (mService != null) {
+				try {
+					Message msg = Message.obtain(null,
+							HardwareController_service.MSG_UNREGISTER_CLIENT);
+					msg.replyTo = mMessenger;
+					mService.send(msg);
+				} catch (RemoteException e) {
+					// There is nothing special we need to do if the service
+					// has crashed.
+				}
+			}
+
+			// Detach our existing connection.
+			unbindService(mConnection);
+			mIsBound = false;
+			Log.d("YOCTOPUS_SERVICE", "unbinding.");
+		}
+	}
 }
