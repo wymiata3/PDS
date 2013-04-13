4ce4abdddc5f88cb471ffff03e53be6e367832a5
chmod <chmod@localhost.localdomain>
2013-04-13 10:54:24 +0200

    Present the characteristics of device rather than hardcoded

--- Voltset/src/com/ku/voltset/InfoDialog.java
+++ Voltset/src/com/ku/voltset/InfoDialog.java
@@ -12,18 +12,30 @@ import android.widget.TextView;
 public class InfoDialog extends DialogFragment {
 	private String serial;
 	private View mRoot;
+	String yocto_serial = null;
+	String LogicalName = null;
+	String Luminosity = null;
+	String UpTime = null;
+	String UsbCurrent = null;
+	String Beacon = null;
 	public InfoDialog(){
 		
 		Bundle arguments=getArguments();
 		this.serial=arguments.getString("serial");
+		this.LogicalName = arguments.getString("LogicalName");
+		this.Luminosity = arguments.getString("Luminosity");
+		this.UpTime = arguments.getString("UpTime");
+		this.UsbCurrent = arguments.getString("UsbCurrent");
+		this.Beacon = arguments.getString("Beacon");
 	}
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		LayoutInflater inflater = getActivity().getLayoutInflater();
 		mRoot = inflater.inflate(R.layout.infodialog, null);
-		TextView serialView=(TextView)mRoot.findViewById(R.id.textView2);
+		TextView serialView=(TextView)mRoot.findViewById(R.id.txtSerialV);
 		serialView.setText(serial);
+		
 		builder.setView(mRoot)
 				.setNegativeButton(R.string.dialog_close,
 						new DialogInterface.OnClickListener() {
