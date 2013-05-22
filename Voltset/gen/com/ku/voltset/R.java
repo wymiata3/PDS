f3651d68801c84868a181e86832cf855461d6d96
kamil <kamil@THINK.home>
2013-05-22 13:14:43 +0200

    nothing changed, but I am doing it for panos :)

--- Voltset/gen/com/ku/voltset/R.java
+++ Voltset/gen/com/ku/voltset/R.java
@@ -41,9 +41,10 @@ public final class R {
         public static final int ic_launcher=0x7f020005;
         public static final int info=0x7f020006;
         public static final int middlelayer=0x7f020007;
-        public static final int settingsicon=0x7f020008;
-        public static final int signalmeter=0x7f020009;
-        public static final int upmeter=0x7f02000a;
+        public static final int purple_btn=0x7f020008;
+        public static final int settingsicon=0x7f020009;
+        public static final int signalmeter=0x7f02000a;
+        public static final int upmeter=0x7f02000b;
     }
     public static final class id {
         public static final int ACDClayout=0x7f0d001d;
